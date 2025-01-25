03b5b3554e49e8a0effd045a4c35e2820bad60c8
oshi187 <oshinividj@gmail.com>
2025-01-25 10:59:51 +0100

    Update exercise38(BisectionMethodModified).c

--- exercise38(BisectionMethodModified).c
+++ exercise38(BisectionMethodModified).c
@@ -8,58 +8,49 @@ Description - This program approximate the roots of the function f(x) using the 
 #include<stdio.h>
 #include<math.h>
 
-double functionPass(double);
+double function(double);
+double bisection(double, double, double, double);
 
-int main() {
-
-	double varXl = 0;
-	double varXr = 4;
-	double varXm = 0;
+int main() 
+{
+	double varXl = 0.0;
+	double varXr = 4.0;
+	double varXm = 0.0;
 	double tolerance = 1e-6;
 
-	double fXl = functionPass(varXl);
-	double fXr = functionPass(varXr);
-
-	if ((varXl * varXr) < 0)
+	if ((function(varXl) * function(varXr)) < 0)
 	{
-		printf("There exist a zero-crossing in the interval %.2lf and %.2lf", varXl, varXr);
-		while ((varXr - varXl) > tolerance)
-		{
-			varXm = (varXl + varXr) / 2;
-			double fXm = functionPass(varXm);
-
-			printf("\nInterval: [%lf, %lf], Midpoint: %lf, f(Xm): %lf\n", varXl, varXr, varXm, fXm);
-
-			if (fXl * fXm < 0)
-			{
-				varXr = varXm;
-				fXr = fXm;
-
-			}
-			else
-			{
-				varXl = varXm;
-				fXr = fXm;
-			}
-
-		}
-		varXm = (varXl + varXr) / 2;			//final midpoint
-		printf("\nApproximation of zero-crossing: x = %.6lf", varXm);
-		printf("\nThe function value at x: f(x) = %.6lf is ", functionPass(varXm));
-
-
+		printf("There exist a zero-crossing in the interval %.2lf and %.2lf", varXl, varXr);	
+		printf("\nApproximation of zero-crossing: x = %.6lf", bisection(varXl, varXr, varXm, tolerance));
+		printf("\nThe function value at x: f(x) = %.6lf is ", function(varXm));
 	}
 	else
 	{
 		printf("There exist NO zero-crossing in the interval  %.2lf and %.2lf", varXl, varXr);
-	}
-	
+	}	
 	return 0;
 }
 
-double functionPass(double x) {
-
+double function(double x) 
+{
 	double function = pow(x, 2) - (2 * x) - 1;
-
 	return function;
+}
+
+double bisection(double xl, double xr, double xm, double tolerance)
+{
+	while (fabs(xl - xr) > tolerance)
+	{
+		xm = (xl + xr) / 2.0;
+		if ((function(xm) * function(xl)) < 0.0)
+		{
+			xr = xm;		
+		}
+		else
+		{
+			xl = xm;
+		}
+	}
+	xm = (xl + xr) / 2;
+	return xm;
 }
